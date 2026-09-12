32fd4b919a3bd2ef4c1a84791ddf35b06c37324a
ym_work <256305102+ymwork-dev@users.noreply.github.com>
2026-09-12 20:17:44 +0900

    ダミーデータ修正

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -29,7 +29,7 @@ class ContactFactory extends Factory
             'gender' => fake()->numberBetween(1, 3),
             'email' => fake()->safeEmail(),
             'tel' => fake()->phoneNumber(),
-            'address' => fake()->address(),
+            'address' => '東京都新宿区西新宿2-8-1',
             'building' => fake()->secondaryAddress(),
             'detail' => fake()->randomElement($this->details),
         ];
